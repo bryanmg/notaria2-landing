182d7bca2247131dba1aad7f3fff04ef5a0f051a
Chofer Pro <choferpro@MacBook-Pro-Witann-01.local>
2019-09-26 16:05:27 -0500

    ir a secciones

---
+++ resources/views/index.blade.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Notaria2|Home</title>
+	
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap" rel="stylesheet">
+	
+	<link href="https://fonts.googleapis.com/css?family=Stint+Ultra+Condensed&display=swap" rel="stylesheet">
+
+    <script type="text/javascript">
+function ver(n) {
+         document.getElementById("subseccion"+n).style.display="block"
+         }
+function ocultar(n) {
+         document.getElementById("subseccion"+n).style.display="none"
+         }
+</script>
+</head>
+<body>
+	<!---HEADER-->
+	<header class="header">
+		<h1>
+			<!--img src="img/inicio.png" alt="inicio.png" width="150"-->
+		</h1>
+	</header>
+	<!---NAVIGATION-->
+	<nav class="navbar">
+		<a href="#Origen">Origen</a>
+			<li><a href="#Asesoria">Asesorías</a></li>
+			<ul>
+				<li><a href="#Asesoria">Asesoría Contable</a></li>
+				<li><a href="#Asesoria">Asesoría Fiscal</a></li>
+				<li><a href="#Asesoria">Auditoria</a></li>
+		</ul>
+		<a href="#Laboral">Laboral</a>
+		<a href="#Servicios">Servicios legales</a>
+		<a href="#Juridico Contencioso">Jurídico contencioso</a>
+		<a href="#Comercio Exterior">Comercio Exterior</a>
+		<a href="#Servicios">Servicios</a>
+	</nav>	
+
+	<!---INICIO-->
+	<section class="content who">
+		<!--article class="contain"--> 
+		<img src="img/inicio.png" alt="inicio.png" width="1250" align="center">
+				<a name="Origen">
+				<h2 class="title">Origen</h2>
+					<div class="vertical-menu">
+ 						<a href="#">ÉTICA</a>
+  						<a href="#">CALIDAD</a>
+  						<a href="#">HONESTIDAD</a>
+  						<a href="#">HONESTIDAD</a>
+				</div>
+				<p>
+					Corporativo Ramos Marrón es una firma de contadores públicos y abogados.........
+				</p>
+				<h2>Objetivos</h2>
+				<p>
+					Eficientar los procesos estratégicos de las empresas a través de nuestros servicios........
+				</p>
+				<h2>Ventajas Competitivas</h2>
+				<p>
+					Las ventajas de nuestra firma consiste en la experiencia profesional de nuestro equipo de trabajo,........
+					
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+			<!--/article-->
+		</section>
+		<!--fin -->
+		
+		<!---ASESORIAS-->	
+		
+		<section class="content services">
+			<a name="Asesoria">
+				<h2 class="title">Asesorías</h2>
+				<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<ul class="list-services">
+					<section>
+						<li>
+						<figure>
+							<img src="img/inicio.png" alt="inicio.png" width="140"  class='imgRedonda'>
+							<figcaption>Asesoria Contable</figcaption>
+						</figure>
+
+						<li>
+					</section>
+					<section>
+						<figure>
+							<img src="img/inicio.png" alt="inicio.png" width="150"  class='imgRedonda'>
+							<figcaption>Asesoria Fiscal</figcaption>
+						</figure>
+					
+						<li>
+					</section>
+					<section>
+						<figure>
+							<img src="img/inicio.png" alt="inicio.png" width="150" class='imgRedonda'>
+							<figcaption>Auditoria</figcaption>
+						</figure>
+						</li>
+					</section>
+				</ul>
+			</section>
+			<section class="content who">
+				<a name="Asesoria Fiscal">
+				<h2 class="title">Asesoría fiscal</h2>
+				<p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</section>
+				<section class="content services">
+				<h2 class="title">Auditoria</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<ul>
+						<li>
+						<figure>
+							<img src="img/inicio.png" alt="inicio.png" width="150"  class='imgRedonda'>
+							<figcaption>Asesoria Contable</figcaption>
+						</figure>
+						<li>
+							<li>
+						<figure>
+							<img src="img/inicio.png" alt="inicio.png" width="150"  class='imgRedonda'>
+							<figcaption>Asesoria Contable</figcaption>
+						</figure>
+						<li>
+							<li>
+						<figure>
+							<img src="img/inicio.png" alt="inicio.png" width="150" class='imgRedonda'>
+							<figcaption>Asesoria Contable</figcaption>
+						</figure>
+						<li>
+					</ul>
+			</div>
+		</section>
+		<!--fin -->
+		<!---LABORAL-->	
+
+			<div class="info">
+				<a name="Laboral">
+				<h2 class="title">Laboral</h2>
+
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<ul>
+						<li>
+						<figure>
+							<img src="img/inicio.png" alt="inicio.png" width="150" class='imgRedonda'>
+							<figcaption>Asesoria Contable</figcaption>
+						</figure>
+						<li>
+							<li>
+						<figure>
+							<img src="img/inicio.png" alt="inicio.png" width="150" class='imgRedonda'>
+							<figcaption>Asesoria Contable</figcaption>
+						</figure>
+						<li>
+							<li>
+						<figure>
+							<img src="img/inicio.png" alt="inicio.png" width="150" class='imgRedonda'>
+							<figcaption>Asesoria Contable</figcaption>
+						</figure>
+						<li>
+					</ul>
+			</div>
+		</article>
+		<!--fin -->
+		<!---	SERVICIOS LEGALES-->	
+		<section class="content services">
+			<div class="info">
+				<a name="Servicios">
+				<h2 class="title">Sevicios legales</h2>
+				<p>
+					Revisión de libros de actas de asamblea de accionistas, de variación de capital, de registro........
+				</p>
+				<ul>
+						<li>
+						<figure>
+							<img src="img/inicio.png" alt="inicio.png" width="150" class='imgRedonda'>
+							<figcaption>Asesoria Contable</figcaption>
+						</figure>
+						<li>
+							<li>
+						<figure>
+							<img src="img/inicio.png" alt="inicio.png" width="150" class='imgRedonda'>
+							<figcaption>Asesoria Contable</figcaption>
+						</figure>
+						<li>
+							<li>
+						<figure>
+							<img src="img/inicio.png" alt="inicio.png" width="150" class='imgRedonda'>
+							<figcaption>Asesoria Contable</figcaption>
+						</figure>
+						<li>
+					</ul>
+			</div>
+		</section>
+		<!--fin -->
+		<!---JURÍDICO CONTENCIOSO-->	
+		<section class="content who">
+			<div class="info">
+				<a name="Juridico Contencioso">
+				<h2 class="title">Jurídico contencioso</h2>
+				<p>
+					Defensoría jurídico fiscal en el ambito federal .......
+				</p>
+				<ul>
+						<li>
+						<figure>
+							<div style="text-align: center"> <img src="img/inicio.png" alt="inicio.png" width="150" class='imgRedonda'></div>
+							<figcaption>Asesoria Contable</figcaption>
+						</figure>
+						<li>
+							<li>
+						<figure>
+							<div style="text-align: center"> <img src="img/inicio.png" alt="inicio.png" width="150" class='imgRedonda'></div>
+							<figcaption>Asesoria Contable</figcaption>
+						</figure>
+						<li>
+							<li>
+						<figure>
+							<div style="text-align: center"> <img src="img/inicio.png" alt="inicio.png" width="150" class='imgRedonda'></div>
+							<figcaption>Asesoria Contable</figcaption>
+						</figure>
+						<li>
+					</ul>
+			</div>
+		</section>
+		<!--fin -->
+		<!---COMERCIO EXTERIOR-->	
+	<section class="content services">
+			<div class="info">
+				<a name="Comercio Exterior">
+				<h2 class="title">Comercio Exterior</h2>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+			<ul>
+						<li>
+						<figure>
+							<div style="text-align: center"> <img src="img/inicio.png" alt="inicio.png" width="150" class='imgRedonda'></div>
+							<figcaption>Asesoria Contable</figcaption>
+						</figure>
+						<li>
+							<li>
+						<figure>
+							<div style="text-align: center"> <img src="img/inicio.png" alt="inicio.png" width="150" class='imgRedonda'></div>
+							<figcaption>Asesoria Fiscal</figcaption>
+						</figure>
+						<li>
+							<li>
+						<figure>
+							<div style="text-align: center"> <img src="img/inicio.png" alt="inicio.png" width="150" class='imgRedonda'></div>
+							<figcaption>Auditoria</figcaption>
+						</figure>
+						<li>
+					</ul>
+		</section>
+		<!--fin -->
+	</section>
+	<footer>
+  <p>&copy; 2019</p>
+  <ul class="list-services">
+    <li><a href="/acerca-de">Acerca de</a></li>
+    <li><a href="/conectar">Conectar</a></li>
+    <li><a href="/rrss">Redes Sociales</a></li>
+  </ul>
+</footer>
+</body>
+</html>
